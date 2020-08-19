c3539d7df9e071ac64ccccf7c3c53e7607a662f1
oleg moroz <oleg.moroz@mcc.vniiem.ru>
2020-08-19 09:27:33 +0300

    Убираем родимые пятна примера

--- aic12202.c
+++ aic12202.c
@@ -19,122 +19,122 @@
 #include <linux/cdev.h>
 #include <asm/io.h>
 
-#define SHORT_NR_PORTS	16	/* use 16 ports by default */
-
+#define AIC12202_NR_PORTS	16	/* use 16 ports by default */
+#define AIC12202_DEVICE_NAME "aic12202"
 
 static unsigned long base = 0x140;
-unsigned long short_base = 0;
+unsigned long aic12202_base = 0;
 module_param(base, long, 0);
 
-static unsigned int cfake_major = 0;
-struct cfake_dev
+static unsigned int aic12202_major = 0;
+struct aic12202_dev
 {
 	struct cdev cdev;
 	int data;
 };
-static struct cfake_dev *cfake_devices = NULL;
-static struct class *cfake_class = NULL;
-static int cfake_ndevices = 1;
+static struct aic12202_dev *aic12202_devices = NULL;
+static struct class *aic12202_class = NULL;
+static int aic12202_ndevices = 1;
 
 MODULE_AUTHOR ("Oleg Moroz");
 MODULE_LICENSE("Dual BSD/GPL");
 
 
-int short_open (struct inode *inode, struct file *filp)
+int aic12202_open (struct inode *inode, struct file *filp)
 {
 	return 0;
 }
 
 
-int short_release (struct inode *inode, struct file *filp)
+int aic12202_release (struct inode *inode, struct file *filp)
 {
 	return 0;
 }
 
 
-ssize_t do_short_read (struct inode *inode, struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+ssize_t do_aic12202_read (struct inode *inode, struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
 	return 0;
 }
 
 
-ssize_t short_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+ssize_t aic12202_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
-	//return do_short_read(filp->f_dentry->d_inode, filp, buf, count, f_pos);
+	//return do_aic12202_read(filp->f_dentry->d_inode, filp, buf, count, f_pos);
 	return 0;
 }
 
 
 
-ssize_t do_short_write (struct inode *inode, struct file *filp, const char __user *buf,
+ssize_t do_aic12202_write (struct inode *inode, struct file *filp, const char __user *buf,
 		size_t count, loff_t *f_pos)
 {
 	return count;
 }
 
 
-ssize_t short_write(struct file *filp, const char __user *buf, size_t count,
+ssize_t aic12202_write(struct file *filp, const char __user *buf, size_t count,
 		loff_t *f_pos)
 {
-	//return do_short_write(filp->f_dentry->d_inode, filp, buf, count, f_pos);
+	//return do_aic12202_write(filp->f_dentry->d_inode, filp, buf, count, f_pos);
 }
 
 
 
-unsigned int short_poll(struct file *filp, poll_table *wait)
+unsigned int aic12202_poll(struct file *filp, poll_table *wait)
 {
 	return POLLIN | POLLRDNORM | POLLOUT | POLLWRNORM;
 }
 
 
-struct file_operations short_fops = {
+struct file_operations aic12202_fops = {
 		.owner	 = THIS_MODULE,
-		.read	 = short_read,
-		.write	 = short_write,
-		.poll	 = short_poll,
-		.open	 = short_open,
-		.release = short_release,
+		.read	 = aic12202_read,
+		.write	 = aic12202_write,
+		.poll	 = aic12202_poll,
+		.open	 = aic12202_open,
+		.release = aic12202_release,
 };
 
 /* then,  the interrupt-related device */
 
-static void cfake_destroy_device(struct cfake_dev *dev, int minor, struct class *class)
+static void aic12202_destroy_device(struct aic12202_dev *dev, int minor, struct class *class)
 {
 	BUG_ON(dev == NULL || class == NULL);
-	device_destroy(class, MKDEV(cfake_major, minor));
+	device_destroy(class, MKDEV(aic12202_major, minor));
 	cdev_del(&dev->cdev);
 	return;
 }
 
-static void cfake_cleanup_module(int devices_to_destroy)
+static void aic12202_cleanup_module(int devices_to_destroy)
 {
 	int i;
 
 	/* Get rid of character devices (if any exist) */
-	if (cfake_devices) {
+	if (aic12202_devices) {
 		for (i = 0; i < devices_to_destroy; ++i) {
-			cfake_destroy_device(&cfake_devices[i], i, cfake_class);
+			aic12202_destroy_device(&aic12202_devices[i], i, aic12202_class);
 		}
-		kfree(cfake_devices);
+		kfree(aic12202_devices);
 	}
 
-	if (cfake_class)
-		class_destroy(cfake_class);
+	if (aic12202_class)
+		class_destroy(aic12202_class);
 
-	/* [NB] cfake_cleanup_module is never called if alloc_chrdev_region()
+	/* [NB] aic12202_cleanup_module is never called if alloc_chrdev_region()
 	 * has failed. */
-	unregister_chrdev_region(MKDEV(cfake_major, 0), cfake_ndevices);
+	unregister_chrdev_region(MKDEV(aic12202_major, 0),aic12202_ndevices);
 	return;
 }
 
 
 
 
-static int cfake_construct_device(struct cfake_dev *dev, int minor,
+static int aic12202_construct_device(struct aic12202_dev *dev, int minor,
 	struct class *class)
 {
 	int err = 0;
-	dev_t devno = MKDEV(cfake_major, minor);
+	dev_t devno = MKDEV(aic12202_major, minor);
 	struct device *device = NULL;
 
 	BUG_ON(dev == NULL || class == NULL);
@@ -143,23 +143,22 @@ static int cfake_construct_device(struct cfake_dev *dev, int minor,
 	dev->data = 0;
 
 
-	cdev_init(&dev->cdev, &short_fops);
+	cdev_init(&dev->cdev, &aic12202_fops);
 	dev->cdev.owner = THIS_MODULE;
 
 	err = cdev_add(&dev->cdev, devno, 1);
 	if (err)
 	{
 		printk(KERN_WARNING "[target] Error %d while trying to add %s%d",
-			err, "short", minor);
+			err, AIC12202_DEVICE_NAME, minor);
 		return err;
 	}
 
-	device = device_create(class, NULL, devno, NULL, "short" "%d", minor);
+	device = device_create(class, NULL, devno, NULL, AIC12202_DEVICE_NAME "%d", minor);
 
 	if (IS_ERR(device)) {
 		err = PTR_ERR(device);
-		printk(KERN_WARNING "[target] Error %d while trying to create %s%d",
-			err, "short", minor);
+		printk(KERN_WARNING "[target] Error %d while trying to create %s%d",err, AIC12202_DEVICE_NAME, minor);
 		cdev_del(&dev->cdev);
 		return err;
 	}
@@ -168,65 +167,65 @@ static int cfake_construct_device(struct cfake_dev *dev, int minor,
 
 /* Finally, init and cleanup */
 
-int short_init(void)
+int aic12202_init(void)
 {
 	int result;
 	int i;
 	int devices_to_destroy = 0;
-	short_base = base;
+	aic12202_base = base;
 
 
-	//release_region(short_base,SHORT_NR_PORTS);
-	if (! request_region(short_base, SHORT_NR_PORTS, "short"))
+	//release_region(aic12202_base,AIC12202_NR_PORTS);
+	if (! request_region(aic12202_base, AIC12202_NR_PORTS, AIC12202_DEVICE_NAME))
 	{
-		printk(KERN_INFO "short: can't get I/O port address 0x%lx\n",short_base);
+		printk(KERN_INFO "%s: can't get I/O port address 0x%lx\n",AIC12202_DEVICE_NAME,aic12202_base);
 		return -EACCES;
 	}
-	  int k =inb(short_base+14);
-	  int n =inb(short_base+15);
+	  int k =inb(aic12202_base+14);
+	  int n =inb(aic12202_base+15);
 	    if (!((k=='C'||k=='V')&&(n==8||n==16))) {
-	    	printk(KERN_INFO "short: ne plata\n",short_base);
+	    	printk(KERN_INFO "%s: ne plata\n",AIC12202_DEVICE_NAME,aic12202_base);
 	    	return -ENODEV;
 	    };
 
 
-	    outb(0x80,short_base+0);
-	    printk(KERN_INFO "short: find device\n");
+	    outb(0x80,aic12202_base+0);
+	    printk(KERN_INFO "%s: find device\n",AIC12202_DEVICE_NAME);
 
 	    dev_t dev = 0;
 
-	    result = alloc_chrdev_region(&dev, 0, 1, "short");
+	    result = alloc_chrdev_region(&dev, 0, 1, AIC12202_DEVICE_NAME);
 
 
 
 		if (result < 0)
 		{
 			printk(KERN_WARNING "[target] alloc_chrdev_region() failed\n");
-			release_region(short_base,SHORT_NR_PORTS);
+			release_region(aic12202_base,AIC12202_NR_PORTS);
 			return result;
 		}
 
-		cfake_major = MAJOR(dev);
-		printk(KERN_INFO "short: major %d\n",cfake_major);
+		aic12202_major = MAJOR(dev);
+		printk(KERN_INFO "%s: major %d\n",AIC12202_DEVICE_NAME,aic12202_major);
 
 
-		cfake_class = class_create(THIS_MODULE, "short");
-		if (IS_ERR(cfake_class)) {
-			result = PTR_ERR(cfake_class);
+		aic12202_class = class_create(THIS_MODULE, AIC12202_DEVICE_NAME);
+		if (IS_ERR(aic12202_class)) {
+			result = PTR_ERR(aic12202_class);
 			goto fail;
 		}
 
 		/* Allocate the array of devices */
-			cfake_devices = (struct cfake_dev *)kzalloc(cfake_ndevices * sizeof(struct cfake_dev),GFP_KERNEL);
-			if (cfake_devices == NULL) 
+			aic12202_devices = (struct aic12202_dev *)kzalloc(aic12202_ndevices * sizeof(struct aic12202_dev),GFP_KERNEL);
+			if (aic12202_devices == NULL) 
 			{
 				result = -ENOMEM;
 				return result;
 			}
 
 			/* Construct devices */
-			for (i = 0; i < cfake_ndevices; ++i) {
-				result = cfake_construct_device(&cfake_devices[i], i, cfake_class);
+			for (i = 0; i < aic12202_ndevices; ++i) {
+				result = aic12202_construct_device(&aic12202_devices[i], i, aic12202_class);
 				if (result) {
 					devices_to_destroy = i;
 					goto fail;
@@ -237,19 +236,19 @@ int short_init(void)
 			
 		
 		fail:
-		cfake_cleanup_module(devices_to_destroy);
-		release_region(short_base,SHORT_NR_PORTS);
+		aic12202_cleanup_module(devices_to_destroy);
+		release_region(aic12202_base,AIC12202_NR_PORTS);
 		return result;
 	}
 
-	void short_cleanup(void)
+	void aic12202_cleanup(void)
 	{
 		/* Make sure we don't leave work queue/tasklet functions running */
-		unregister_chrdev(cfake_major, "short");
-		release_region(short_base,SHORT_NR_PORTS);
-		cfake_cleanup_module(1);
+		unregister_chrdev(aic12202_major, AIC12202_DEVICE_NAME);
+		release_region(aic12202_base,AIC12202_NR_PORTS);
+		aic12202_cleanup_module(1);
 		
 	}
 
-	module_init(short_init);
-	module_exit(short_cleanup);
+	module_init(aic12202_init);
+	module_exit(aic12202_cleanup);
